c4aa92b76d785fada26ae44951f625416458a098
Hector <hectoraltamira1ai@gmail.com>
2024-09-04 16:05:24 -0700

    minor change in the import order

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ImageComponent from './components/ImageComponent';
-import './App.css';
 import image1 from './images/image1.jpg';
 import image2 from './images/image2.jpg';
 import image3 from './images/image3.jpg';
 import image4 from './images/image4.jpg';
 import image5 from './images/image5.jpg';
 import image6 from './images/image6.jpg';
+import './App.css';
+
 
 const App = () => {
   const images = [
